a51e4e9f7aa45e369b6e576f19050f6d78080199
Lukas Woodtli <woodtli.lukas@gmail.com>
2016-06-26 11:02:03 +0200

    Work on tutorial

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,6 +3,8 @@ __author__ = 'Lukas Woodtli'
 from django.conf.urls import url
 from . import views
 
+app_name = 'polls'
+
 urlpatterns = [
     # ex: /polls/
     url(r'^$', views.index, name='index'),
